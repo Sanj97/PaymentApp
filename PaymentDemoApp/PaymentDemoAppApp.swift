d6dc240d6ceb144ee1a88bfc4ca7e6fb90f82ebd
Sanjar Yalgashev <skuralov97@gmail.com>
2025-05-08 11:36:10 +0500

    Initial Commit

---
+++ PaymentDemoApp/PaymentDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentDemoAppApp.swift
+//  PaymentDemoApp
+//
+//  Created by Sanjar Yalgashev on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PaymentDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PaymentDemoAppTests/PaymentDemoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentDemoAppTests.swift
+//  PaymentDemoAppTests
+//
+//  Created by Sanjar Yalgashev on 08/05/25.
+//
+
+import Testing
+@testable import PaymentDemoApp
+
+struct PaymentDemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
